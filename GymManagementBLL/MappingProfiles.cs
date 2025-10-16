62f69c89fa1411fa02474235a63c47df1a103b0f
abdogaber10 <hazogaber10@gmail.com>
2025-10-16 13:43:57 +0300

    04

--- GymManagementBLL/MappingProfiles.cs
+++ GymManagementBLL/MappingProfiles.cs
@@ -20,6 +20,7 @@ namespace GymManagementBLL
                      .ForMember(dest => dest.TrainerName, Options => Options.MapFrom(Src => Src.SessionTrainer.Name))
                      .ForMember(dest => dest.AvailableSlots, Options => Options.Ignore());
 
+            //Create
             CreateMap<CreateSessionViewModel, Session>();
 
            // Update 
